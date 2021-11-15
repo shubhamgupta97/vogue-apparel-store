94d4391495e1a13a47b44d240ab8fbc508a1cafb
Shubham Gupta <gupta.shubham97@gmail.com>
2021-11-15 16:59:38 +0530

    Modify controller to generate content based on user preference

--- src/main/java/co/shubhamgupta/vogue/controller/ProductController.java
+++ src/main/java/co/shubhamgupta/vogue/controller/ProductController.java
@@ -3,18 +3,19 @@ package co.shubhamgupta.vogue.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import co.shubhamgupta.vogue.model.UserPrincipal;
 import co.shubhamgupta.vogue.service.ProductService;
 
 @Controller
 public class ProductController {
 
 	private final ProductService productService;
-//	private List<Product> products;
 
 	@Autowired
 	public ProductController(ProductService productService) {
@@ -24,18 +25,24 @@ public class ProductController {
 	@GetMapping(value = "/")
 	public String renderIndex(@RequestParam("q") Optional<String> q, @RequestParam("v") Optional<String> v,
 			Model model) {
-		// products = productService.getProducts();
 
-		if (q.isPresent()) {
+		UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		model.addAttribute("username", principal.getUsername());
+
+		char gender = principal.getGender();
+
+		if (q.isPresent())
 			processQueryParam(q, v, model);
-		} else
-			model.addAttribute("products", productService.getProducts());
+		else if(gender == 'M')
+			model.addAttribute("products", productService.getProductsSortedByGender('M'));
+		else
+			model.addAttribute("products", productService.getProductsSortedByGender('W'));
 
 		return "index";
 	}
 
 	@GetMapping(value = "/search")
-	public String renderIndexOnSearch(@RequestParam("q") Optional<String> q,Model model) {
+	public String renderIndexOnSearch(@RequestParam("q") Optional<String> q, Model model) {
 
 		if (q.isPresent()) {
 			String qValue = q.get();
@@ -45,7 +52,7 @@ public class ProductController {
 
 		return "index";
 	}
-	
+
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
